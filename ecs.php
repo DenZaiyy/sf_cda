95886bfb80ca9ea1eb38655632865a29c34d4d33
DenZaiyy <denzaiyy@outlook.fr>
2025-07-21 16:54:43 +0200

    feat: add rector, ecs and phpstan config

---
+++ ecs.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Operator\ConcatSpaceFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPreparedSets(true)
+    ->withSkip([
+        ConcatSpaceFixer::class
+    ]);
