f7764c03d7d7c8ac9ac355ccc5b89d48ecb2750e
DenZaiyy <denzaiyy@outlook.fr>
2025-07-21 15:34:40 +0200

    feat: add profile page

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/profile', name: 'app.profile.')]
+#[IsGranted('ROLE_USER', message: 'You are not logged in.', statusCode: Response::HTTP_FORBIDDEN)]
+final class ProfileController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+}
