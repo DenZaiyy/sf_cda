0a3a31365ae13917400be9802beea2c361fc3e1a
DenZaiyy <denzaiyy@outlook.fr>
2025-08-18 14:04:21 +0200

    feat: add honeypot subscriber for register

--- src/Form/RegistrationForm.php
+++ src/Form/RegistrationForm.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\EventSubscriber\HoneypotSubscriber;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -69,6 +71,14 @@ class RegistrationForm extends AbstractType
                     ])
                 ],
             ])
+            ->add('website', TextType::class, [
+                'mapped' => false,
+                'required' => false,
+                'label' => false,
+                'attr' => [
+                    'style' => 'display:none'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'label' => 'I agree to the terms and conditions',
@@ -81,7 +91,9 @@ class RegistrationForm extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
-            ]);
+            ])
+            ->addEventSubscriber(new HoneypotSubscriber())
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
