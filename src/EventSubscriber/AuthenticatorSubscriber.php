36e9098027c36a99b67c5552a7e9381cfdd228bf
DenZaiyy <denzaiyy@outlook.fr>
2026-02-26 16:33:55 +0100

    Remove unused Request import
    Delete unused `Request` use statement from src/EventSubscriber/AuthenticatorSubscriber.php to clean up the codebase and avoid unused-import warnings; no functional changes.

--- src/EventSubscriber/AuthenticatorSubscriber.php
+++ src/EventSubscriber/AuthenticatorSubscriber.php
@@ -6,7 +6,6 @@ use App\Entity\User;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
